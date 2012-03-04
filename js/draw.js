5b7de42fc3c0d2480a07407d44b9a232660c19e9
Zation <zation1@gmail.com>
2012-03-04 23:13:21 +0800

    seperate the operation on mobile and desktop

--- js/draw.js
+++ js/draw.js
@@ -2,14 +2,15 @@ window.addEventListener('DOMContentLoaded', function () {
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
   var is_drawing = false;
-  var is_on_canvas = false;
+  var is_in_canvas = false;
+  var is_on_mobile = navigator.userAgent.match(/Mobile/i);
 
   function get_current_x(event) {
-    return event.offsetX ? event.offsetX : event.targetTouches[0].pageX;
+    return event.offsetX === undefined ? event.targetTouches[0].pageX : event.offsetX;
   }
 
   function get_current_y(event) {
-    return event.offsetY ? event.offsetY : event.targetTouches[0].pageY;
+    return event.offsetY === undefined ? event.targetTouches[0].pageY : event.offsetY;
   }
 
   function start_drawing() {
@@ -18,42 +19,43 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   function process_drawing(event) {
-    if (is_drawing && is_on_canvas) {
+    if (is_drawing && is_in_canvas) {
       context.lineTo(get_current_x(event), get_current_y(event));
       context.stroke();
     }
   }
 
   function end_drawing() {
-    is_drawing ? context.closePath() : null;
+    if (is_drawing) context.closePath();
     is_drawing = false;
   }
 
   function leave_canvas() {
-    is_drawing ? context.closePath() : null;
-    is_on_canvas = false;
+    if (is_drawing) context.closePath();
+    is_in_canvas = false;
   }
 
   function enter_canvas() {
-    is_drawing ? context.beginPath() : null;
-    is_on_canvas = true;
+    if (is_drawing) context.beginPath();
+    is_in_canvas = true;
   }
 
-  document.addEventListener('touchmove', function (event) {
-    event.preventDefault();
-  }, false);
+  if (is_on_mobile) {
+    is_in_canvas = true;
+    document.addEventListener('touchmove', function (event) {
+      event.preventDefault();
+    }, false);
+    document.addEventListener('touchstart', start_drawing, false);
+    canvas.addEventListener('touchmove', process_drawing, false);
+    document.addEventListener('touchend', end_drawing, false);
+  }
+  else {
+    document.addEventListener('mousedown', start_drawing, false);
+    canvas.addEventListener('mousemove', process_drawing, false);
+    canvas.addEventListener('mouseout', leave_canvas, false);
+    canvas.addEventListener('mouseover', enter_canvas, false);
+    document.addEventListener('mouseup', end_drawing, false);
+  }
 
-  document.addEventListener('mousedown', start_drawing, false);
-  document.addEventListener('touchstart', start_drawing, false);
-
-  canvas.addEventListener('mousemove', process_drawing, false);
-  canvas.addEventListener('touchmove', process_drawing, false);
-
-  canvas.addEventListener('mouseout', leave_canvas, false);
-
-  canvas.addEventListener('mouseover', enter_canvas, false);
-
-  document.addEventListener('mouseup', end_drawing, false);
-  document.addEventListener('touchend', end_drawing, false);
 
 }, false);

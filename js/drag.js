c37f4e0b6771f2050904fabb548d3aceccd1d98e
Zation <zation1@gmail.com>
2012-03-09 00:21:38 +0800

    add drogging base on the document mousemove event

--- js/drag.js
+++ js/drag.js
@@ -1,13 +1,28 @@
 function initialize_dragging() {
+  var current_dragged_item;
+  var offset_x = 0;
+  var offset_y = 0;
 
-  document.getElementsByClassName('images-gallery')[0].addEventListener('dragstart', function (event) {
-    event.dataTransfer.effectAllowed = 'move';
-  }, false);
+  $('.images-gallery').on('mousedown', 'li', function (event) {
+    current_dragged_item = $(this);
+    offset_x = event.offsetX;
+    offset_y = event.offsetY;
+    event.preventDefault();
+  });
 
-  document.getElementsByClassName('images-gallery')[0].addEventListener('dragend', function (event) {
-    var element = event.target.parentElement;
-    element.className = 'dragged';
-    element.style.top = event.clientY + 'px';
-    element.style.left = event.clientX + 'px';
-  }, false);
+  $(document).on('mousemove', function (event) {
+    if (current_dragged_item) {
+      current_dragged_item.css({
+        'position': 'absolute',
+        'top': event.clientY - offset_y,
+        'left': event.clientX - offset_x
+      });
+    }
+  });
+
+  $(document).on('mouseup', function () {
+    if (current_dragged_item) {
+      current_dragged_item = undefined;
+    }
+  });
 }

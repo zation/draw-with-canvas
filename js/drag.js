c0a3cd66af2f85cd97fda1ae44cccdc0095d1632
Zation <zation1@gmail.com>
2012-03-08 23:06:46 +0800

    add drag event with html5

--- js/drag.js
+++ js/drag.js
@@ -1,3 +1,13 @@
 function initialize_dragging() {
 
+  document.getElementsByClassName('images-gallery')[0].addEventListener('dragstart', function (event) {
+    event.dataTransfer.effectAllowed = 'move';
+  }, false);
+
+  document.getElementsByClassName('images-gallery')[0].addEventListener('dragend', function (event) {
+    var element = event.target.parentElement;
+    element.className = 'dragged';
+    element.style.top = event.clientY + 'px';
+    element.style.left = event.clientX + 'px';
+  }, false);
 }

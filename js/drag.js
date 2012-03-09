70ccfb4b015ec6662cedc2e82dfe4cd94cb84c66
Zation <zation1@gmail.com>
2012-03-10 00:37:54 +0800

    restore image to gallery when drag back

--- js/drag.js
+++ js/drag.js
@@ -13,14 +13,29 @@ function initialize_dragging() {
     event.preventDefault();
   });
 
+  function get_place_holder_index() {
+    var place_holder_index = 0;
+    var all_gallery_image = gallery.find('li');
+
+    for (var index = 0; index < all_gallery_image.length; index++) {
+      var image = $(all_gallery_image[index]);
+      if (image.hasClass('gallery-image')) {
+        place_holder_index++;
+      }
+      else if (image.attr('id') == 'place_holder') {
+        return place_holder_index;
+      }
+    }
+    return -1;
+  }
+
   $(document).on('mousemove', function (event) {
     if (current_dragged_item) {
       var top = event.clientY - offset_y;
       var left = event.clientX - offset_x;
       var right = left - current_dragged_item.outerWidth();
-      current_dragged_item.removeClass('gallery-image');
+      current_dragged_item[0].className = 'dragging';
       current_dragged_item.css({
-        'position': 'absolute',
         'top': top,
         'left': left
       });
@@ -38,22 +53,6 @@ function initialize_dragging() {
         return start_index;
       }
 
-      function get_place_holder_index() {
-        var place_holder_index = 0;
-        var all_gallery_image = gallery.find('li');
-
-        for (var index = 0; index < all_gallery_image.length; index++) {
-          var image = $(all_gallery_image[index]);
-          if (image.hasClass('gallery-image')) {
-            place_holder_index++;
-          }
-          else if (image.attr('id') == 'place_holder') {
-            return place_holder_index;
-          }
-        }
-        return -1;
-      }
-
       function add_place_holder() {
         var gallery_images = $('.gallery-image');
         var insert_index = get_insert_index(gallery_images);
@@ -67,13 +66,19 @@ function initialize_dragging() {
         place_holder.find('img').attr('src', current_dragged_item.find('img').attr('src'));
         add_place_holder();
       }
-    }
 
-    right < gallery_left ? show_place_holder() : place_holder.remove();
+      right < gallery_left ? show_place_holder() : place_holder.remove();
+    }
   });
 
   $(document).on('mouseup', function () {
     if (current_dragged_item) {
+      if (get_place_holder_index() != -1) {
+        current_dragged_item.remove();
+        place_holder.after(current_dragged_item);
+        place_holder.remove();
+        current_dragged_item[0].className = 'gallery-image';
+      }
       current_dragged_item = undefined;
     }
   });
